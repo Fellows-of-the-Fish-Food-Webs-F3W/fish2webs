586eb13a6c214a27a6d4a53f902e8512d98cb793
zool1262 <zool1262@BIO-M-DT-02702.local>
2026-06-12 17:04:56 +0100

    Update NAMESPACE.

---
+++ R/fish2webs-package.R
@@ -0,0 +1,12 @@
+#' fish2webs package
+#'
+#' @keywords internal
+#' @import dplyr
+#' @import tidyr
+#' @import tibble
+#' @import purrr
+#' @import stringr
+#' @import pkgfilecache
+#' @import fishdatabuilder
+#' @import foodwebbuilder
+"_PACKAGE"
